0586a82687d9255e554a4540fd4e90f0acff4025
thinc <45918719+shaoshenchen@users.noreply.github.com>
2021-05-27 22:59:38 +0800

    Add files via upload

---
+++ det.sh
@@ -0,0 +1,6 @@
+python tools/infer/predict_system.py \
+  --image_dir="./img/hh.png" \
+  --rec_model_dir="./infer_model/mobile_rec_infer" \
+  --det_model_dir="./infer_model/mobile_det_infer" \
+  --cls_model_dir="./infer_model/mobile_cls_infer" \
+  --use_space_char=False
---
+++ rec.sh
@@ -0,0 +1,4 @@
+python tools/infer/predict_rec.py \
+  --image_dir="./img/test" \
+  --rec_model_dir="./infer_model/mobile_rec_infer" \
+  --use_space_char=False
